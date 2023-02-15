226584c69191ce7733bd29313704370c83cd2bba
Tsubakurouuu <yzpolice2896@icloud.com>
2023-02-15 10:03:19 +0900

    Spring Freamwork超入門 ~やさしくわかるWEBアプリ開発~ 12章 12-2

--- src/main/java/com/example/quiz/controller/QuizController.java
+++ src/main/java/com/example/quiz/controller/QuizController.java
@@ -1,11 +1,19 @@
 package com.example.quiz.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.quiz.entity.Quiz;
 import com.example.quiz.form.QuizForm;
@@ -37,5 +45,79 @@ public class QuizController {
 		model.addAttribute("title", "登録用フォーム");
 		return "crud";
 	}
+	
+	@PostMapping("/insert")
+	public String insert(@Validated QuizForm quizForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+		Quiz quiz = new Quiz();
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		
+		if(!bindingResult.hasErrors()) {
+			service.insertQuiz(quiz);
+			redirectAttributes.addFlashAttribute("complete", "登録が完了しました");
+			return "redirect:/quiz";
+		} else {
+			return showList(quizForm, model);
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public String showUpdate(QuizForm quizForm, @PathVariable Integer id, Model model) {
+		Optional<Quiz> quizOpt = service.selectOneById(id);
+		Optional<QuizForm> quizFormOpt = quizOpt.map(t -> makeQuizForm(t));
+		if(quizFormOpt.isPresent()) {
+			quizForm = quizFormOpt.get();
+		}
+		makeUpdateModel(quizForm, model);
+		return "crud";
+	}
+	
+	private void makeUpdateModel(QuizForm quizForm, Model model) {
+		model.addAttribute("id", quizForm.getId());
+		quizForm.setNewQuiz(false);
+		model.addAttribute("quizForm", quizForm);
+		model.addAttribute("title", "更新用フォーム");
+	}
+	
+	@PostMapping("/update")
+	public String update(@Validated QuizForm quizForm, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		Quiz quiz = makeQuiz(quizForm);
+		
+		if(!result.hasErrors()) {
+			service.updateQuiz(quiz);
+			redirectAttributes.addFlashAttribute("complete", "更新が完了しました");
+			return "redirect:/quiz/" + quiz.getId();
+		} else {
+			makeUpdateModel(quizForm, model);
+			return "crud";
+		}
+	}
+	
+	private Quiz makeQuiz(QuizForm quizForm) {
+		Quiz quiz = new Quiz();
+		quiz.setId(quizForm.getId());
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		return quiz;
+	}
+	
+	private QuizForm makeQuizForm(Quiz quiz) {
+		QuizForm form = new QuizForm();
+		form.setId(quiz.getId());
+		form.setQuestion(quiz.getQuestion());
+		form.setAnswer(quiz.getAnswer());
+		form.setAuthor(quiz.getAuthor());
+		form.setNewQuiz(false);
+		return form;
+	}
+	
+	@PostMapping("/delete")
+	public String delete(@RequestParam("id") String id, Model model, RedirectAttributes redirectAttributes) {
+		service.deleteQuizById(Integer.parseInt(id));
+		redirectAttributes.addFlashAttribute("delcomplete", "削除が完了しました");
+		return "redirect:/quiz";
 
+	}
 }
